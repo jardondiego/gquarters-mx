1134eb0e4b0e3c2926632e68bca4eb0a8ad4e850
Diego Jardon <diegojardonc@gmail.com>
2021-04-28 12:18:27 -0500

    Add: create services module

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import globals from "./globals";
+
+const productionConfig = JSON.parse(
+  process.env.REACT_APP_FIREBASE_CONFIG_PRODUCTION
+);
+const stagingConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG_STAGING);
+const config = globals.isProduction ? productionConfig : stagingConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;
---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export { default as firebase } from './firebase';
+export { default as api } from './api';
+export { default as globals } from './globals';
