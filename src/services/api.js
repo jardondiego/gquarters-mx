e18e175a9a3a2a559fafbaa9d8234482c32e3ea5
Diego Jardon <diegojardonc@gmail.com>
2021-04-29 16:58:33 -0500

    Add: method for notif subscription

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
-import globals from './globals';
+import globals from "./globals";
 
 export default class API {
   static async getStationsAvailability() {
     return axios.get(`${globals.apiUrl}/stations/getAvailability`);
+  }
+
+  static async subscribeForNotifications(token) {
+    return axios.post(`${globals.apiUrl}/stations/subscribeOnAvailable`, {
+      token,
+    });
   }
 }
